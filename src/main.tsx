d21397c3ba68715113eebde342cde6e47b4450e1
AlexNieblas10 <elalexnieblas@gmail.com>
2024-12-09 18:49:40 -0700

    Routes added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import "./index.css"
+import { App } from './App'
+import { GuessNote } from './views/GuessNote'
+import './index.css'
+import { GlobalProvider } from './context/GlobalProvider'
+import { GuessNoteProvider } from './context/GuessNoteProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+
+          <Route
+            path="guessNote"
+            element={
+              <GuessNoteProvider>
+                <GuessNote />
+              </GuessNoteProvider>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
+  </StrictMode>
 )
