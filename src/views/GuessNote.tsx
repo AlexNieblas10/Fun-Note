fa8d0939870d809b046968a2b3b9c965020ee41c
AlexNieblas10 <elalexnieblas@gmail.com>
2024-12-14 21:09:17 -0700

    View guessNote modified with the scoreboard

--- src/views/GuessNote.tsx
+++ src/views/GuessNote.tsx
@@ -1,9 +1,11 @@
 import { Piano } from '../components/Piano'
+import { Scoreboard } from '../components/Scoreboard'
 import { SheetMusic } from '../components/SheetMusic'
 
 export function GuessNote() {
   return (
     <main className="flex flex-col items-center w-full">
+      <Scoreboard />
       <SheetMusic />
       <Piano />
     </main>
